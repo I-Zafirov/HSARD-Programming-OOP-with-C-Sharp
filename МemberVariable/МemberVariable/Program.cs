f2a71cddd1973e5194f7dc55dbc70a913b4ec8e2
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-09 18:03:30 +0200

    Add files via upload

---
+++ МemberVariable/МemberVariable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace МemberVariable
+{
+    class Student
+    {
+        private string mFirstName;
+        private string mLastName;
+        private string mStudentld;
+        private int mCourse = 1;
+        private string mSpeciality;
+        private Course[] mCoursesTaken;
+
+        //Avoid missing the visibility modifier
+        string mRemarks = "(няма забележки)";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
