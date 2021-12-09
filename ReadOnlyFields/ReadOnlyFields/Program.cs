9cde115868af55ed8d8ad3ce1e1c3bad0478d564
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-09 18:16:07 +0200

    Add files via upload

---
+++ ReadOnlyFields/ReadOnlyFields/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadOnlyFields
+{
+    class ReadOnlyDemo
+    {
+        private readonly int mSize;
+        public ReadOnlyDemo(int aSize)
+        {
+            mSize = aSize; //cannot be further modified!
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
