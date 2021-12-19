b4110b94bdb9a722e3b73405c577d82f3a9d23fc
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-19 16:03:27 +0200

    Add files via upload

---
+++ StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Bulgaria
+    {
+        private static int mNumberOfCities = 267; //static field
+        public static int NumberOfCities; //static property
+        {
+            get
+            {
+                return mNumberOfCities;
+            }
+        }
+        public static void AddCity(string aCityName) //static method
+        {
+            mNumberOfCities++;
+            //...
+        }
+
+        //...
+ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("In Bulgaria have {0} cities.", Bulgaria.NumberOfCities);
+        }
+    }
+
