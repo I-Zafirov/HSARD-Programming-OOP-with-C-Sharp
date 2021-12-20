70cf1f4d94f495716de811e8f86e7aa9a6389be5
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-20 19:42:45 +0200

    Add files via upload

---
+++ ExampleActivationOfDefaultConstructorOfPredecessor/ExampleActivationOfDefaultConstructorOfPredecessor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExampleActivationOfDefaultConstructorOfPredecessor
+{
+    public class Name
+    {
+        public Name() { myName = 0; }
+        //...
+    }
+    public class Contact:Name
+    {
+        public Contact() { myAddress = 0; }
+        public Contact() : base() { myAddress = 0; }
+        //...
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact Cont = new Contact(); //Activation default constructor.
+            //...
+        }
+    }
+}
