4600c77a25eb77df3cf467b0ecfacfbb2158f1a9
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-20 19:59:00 +0200

    Add files via upload

---
+++ ExampleNestedClasses/ExampleNestedClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExampleNestedClasses
+{
+    public class Container
+    {
+        public class Nasted //If it is not public it will not be visible
+                            //and accessible from the outside.
+        {
+            private Container parent;//To use the components of the outer class.
+            public Nested() { }
+            public Nested(Container parent)
+            {
+                this.parent = parent;
+            }
+        }
+    }
+    class Test
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an exemplar using its full name.
+            Container.Nested nest = new Container.Nasted();
+            //Wrong:
+            //Nested nest = new Nested();
+        }
+    }
+}
