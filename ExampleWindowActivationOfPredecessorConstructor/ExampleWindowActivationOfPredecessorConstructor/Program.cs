c5b03d20fc9b322765dd78be2ea74d9090ea74f9
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-20 19:32:44 +0200

    Add files via upload

---
+++ ExampleWindowActivationOfPredecessorConstructor/ExampleWindowActivationOfPredecessorConstructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExampleWindowActivationOfPredecessorConstructor
+{
+    //Declaring class Window.
+    public class Window
+    {
+        public Window(Rect rSize) {/*... */}
+    }
+    //Declaring class DialogBox, inheritor of class Window.
+    public class DialogBox:Window
+    {
+        //Defines a constructor for DialogBox.
+        //This constructor requires the predecessor constructor Windows.
+        public DialogBox(Rect rSize):base(rSize)
+        { /*...*/}
+        //Exemplars creation and calling constructors.
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling constructors.
+            DialogBox DB = new DialogBox(rS);
+            //...
+        }
+    }
+}
