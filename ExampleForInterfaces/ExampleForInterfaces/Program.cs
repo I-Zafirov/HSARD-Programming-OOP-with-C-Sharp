44f5844993137c1e90adcd9c9b8601a5c0e7911c
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-21 16:43:25 +0200

    Add files via upload

---
+++ ExampleForInterfaces/ExampleForInterfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExampleForInterfaces
+{
+    interface IMovable
+    {
+        void Move(int aDeltaX, int aDeltaY);
+    }
+    interface IShape
+    {
+        void SetPosition(int aX, int aY);
+        double CalculateSurface();
+    }
+    interface IPerimeterShape:IShape
+    {
+        double CalculatePerimeter();
+    }
+    interface IResizable
+    {
+        void Resize(int aWeight);
+        void Resize(int aWeight, int aWeightY);
+        void ResizeByX(int aWeightX);
+        void ResizeByY(int aWeightY);
+    }
+    interface IDrawableShape : IShape, IResizable, IMovable
+    {
+        void Delete();
+        Color Color
+        {
+            get;
+            set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //...
+        }
+    }
+}
