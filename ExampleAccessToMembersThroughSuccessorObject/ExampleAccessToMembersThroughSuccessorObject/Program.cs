1be2e8ef84164e81ba04fada931e2c48aa3462ed
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-12-21 16:17:19 +0200

    Add files via upload

---
+++ ExampleAccessToMembersThroughSuccessorObject/ExampleAccessToMembersThroughSuccessorObject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExampleAccessToMembersThroughSuccessorObject
+{
+    public class Countainer
+    {
+        public class Name
+        {
+            public Name() { }
+            ~Name() { }
+            public void SetName(string n) { }
+            public void Print()
+            {
+                Console.WriteLine(myName);
+            }
+            private string myName = "ime";
+        }
+        public class Contact:Name
+        {
+            public Contact() { }
+            ~Contact() { }
+            public void SetAddress(string c) { }
+            public void Print()
+            {
+                base.Print();
+                Console.WriteLine(myAddress);
+            }
+            private string myAddress = "adres";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact Cont = new Contact();
+            Cont.Print();
+            Cont.SetAddress("Plovdiv");
+            ((Name)Cont).Print();
+        }
+    }
+}
