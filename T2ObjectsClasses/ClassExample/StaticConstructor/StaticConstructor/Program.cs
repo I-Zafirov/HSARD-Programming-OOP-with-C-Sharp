319ce1cfb29a3924b84edf8e6b9b85e38c8ee746
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2022-05-17 09:41:25 +0300

    Add files via upload

---
+++ T2ObjectsClasses/ClassExample/StaticConstructor/StaticConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    class SqrtPrecalculated
+    {
+        public const int MAX_VALUE = 1000;
+        private static int[] mSqrtValues; //static field
+        //Static constructor
+        static SqrtPrecalculated()
+        {
+            mSqrtValues = new int[MAX_VALUE + 1];
+            for (int i = 0; i <= MAX_VALUE; i++)
+                mSqrtValues[i] = (int)Math.Sqrt(i);
+        }
+        //Static method
+        public static int GetSqrt(int aValue)
+        {
+            return mSqrtValues[aValue];
+        }
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(GetSqrt(1000));
+            }
+        }
+    }
+}
