009342ffdafa03b694bccb235aaf517e649e3ccc
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2022-05-17 09:33:31 +0300

    Add files via upload

---
+++ T2ObjectsClasses/ClassExample/ConvertRationalNumber/ConvertRationalNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConvertRationalNumber
+{
+    class TRacional //defines a class
+    {
+        private int mNumber, mDenom; //numerator and denominator field
+        private bool mPositive; //sign number
+
+        public TRacional (int N, int D)
+        {
+            if (D == 0)
+            {
+                Console.WriteLine("Error: Rational number with denominator 0!");
+            }
+            else
+            {
+                mNumber = Math.Abs(N);
+                mDenom = Math.Abs(D);
+                mPositive = (N * D) >= 0;
+            }
+        }
+        public int Number
+        {//return value of number
+            get
+            {
+                return mNumber;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
