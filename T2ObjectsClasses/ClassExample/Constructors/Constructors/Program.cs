6075bfe1f90a79cc877bc57ebb96eb4b96c3d639
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2022-05-17 09:33:00 +0300

    Add files via upload

---
+++ T2ObjectsClasses/ClassExample/Constructors/Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Student
+    {
+        private string mName;
+        private int mStudentld;
+        private string mPosition = "Student";
+        public Student(string aName, int aStudentld)
+        {
+            mName = aName;
+            mStudentld = aStudentld;
+        }
+        public Student(string aName) : this(aName, -1)
+        {
+
+        }
+
+        public static void Main()
+        {
+            Student s = new Student("Kiro", 12345);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
